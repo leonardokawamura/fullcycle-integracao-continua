7379463cb0ccb00bf59b2fd14b9ee5c4b13770cc
leonardo kawamura <leonardokawamura@gmail.com>
2021-02-20 18:31:15 -0300

    ci: add github actions for every push

--- math_test.go
+++ math_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 	if total != 30 {
-		t.ErrorF("Resultado da soma é inválido: Resultado %d, Esperado: %d", total, 30)
+		t.Errorf("Resultado da soma é inválido: Resultado %d, Esperado: %d", total, 30)
 	}
 }
