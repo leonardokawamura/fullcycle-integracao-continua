668e00060d63243031ae287c7b37a5231e824370
leonardo kawamura <leonardokawamura@gmail.com>
2021-02-20 18:07:51 -0300

    feat: :rocket: adicionado funcao de  soma

---
+++ math_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	total := Soma(15, 15)
+	if total != 30 {
+		t.ErrorF("Resultado da soma é inválido: Resultado %d, Esperado: %d", total, 30)
+	}
+}
